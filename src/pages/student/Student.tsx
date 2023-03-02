9f9ab1581fe6d63d24dc3036427afaf97828856b
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-03-02 18:57:04 +1100

    Add fields to input first name and id

--- src/pages/student/Student.tsx
+++ src/pages/student/Student.tsx
@@ -1,16 +1,53 @@
+import { useState } from "react";
 import { useStudentService } from "../../contexts/StudentProvider";
 import { BookAndCollegeDetails } from "../../models/BookAndCollegeDetails";
 
 const Student = () => {
+    const [firstName, setFirstName] = useState("");
+    const [id, setId] = useState("");
+
     const {bookAndCollegeDets, callBookAndCollegeDetails} = useStudentService();
 
-    const getBookAndCollegeDetails = (studentFirstName: string, studentId: string): BookAndCollegeDetails => {
-        callBookAndCollegeDetails({ studentFirstName, studentId });
-        return bookAndCollegeDets!;
+    const updateFirstName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setFirstName(e.target.value);
+    };
+
+    const updateId = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setId(e.target.value);
     };
 
     return <div>
-        {JSON.stringify(getBookAndCollegeDetails("Kiran", "S1001"))}
+        <div>
+            <label>Student first name</label>
+            <input
+                type="text"
+                value={firstName}
+                onChange={updateFirstName}
+            />
+        </div>
+
+        <div>
+            <label>Student id</label>
+            <input
+                type="text"
+                value={id}
+                onChange={updateId}
+            />
+        </div>
+
+        <div>
+            <input
+                type="button"
+                onClick={() => callBookAndCollegeDetails({studentFirstName: firstName, studentId: id})}
+                value="Submit"
+            />
+        </div>
+
+        <code>
+            {JSON.stringify(bookAndCollegeDets!)}
+        </code>
     </div>;
 }
 
