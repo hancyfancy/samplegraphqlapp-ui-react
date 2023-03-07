55aef21f93ab67fbcc6578815c67dbe7b86a03fe
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-03-08 10:30:53 +1100

    Return stateful jsx element

--- src/components/student/StudentCollection.tsx
+++ src/components/student/StudentCollection.tsx
@@ -1,32 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useStudentService } from "../../contexts/StudentProvider";
 
 const StudentCollection = () => {
     const {callStudents, studnts} = useStudentService();
+    const [table, setTable] = useState<JSX.Element | undefined>(undefined);
 
     useEffect(() => {
         callStudents();
     }, []);
 
-    return studnts !== undefined ? <table>
-        <thead>
-            <tr>
-                <th>FirstName</th>
-                <th>LastName</th>
-                <th>Email</th>
-            </tr>
-        </thead>
-        <tbody>
-            {studnts!.map((elem, index) => {
-                return <tr key={elem.id}>
-                    <td>{elem.firstName}</td>
-                    <td>{elem.lastName}</td>
-                    <td>{elem.email}</td>
-                </tr>;
-            })}
-        </tbody>
-    </table>
-    : <></>;
+    useEffect(() => {
+        setTable(studnts !== undefined
+            ? <table>
+                <thead>
+                    <tr>
+                        <th>FirstName</th>
+                        <th>LastName</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {studnts!.map((elem, index) => {
+                        return <tr key={elem.id}>
+                            <td>{elem.firstName}</td>
+                            <td>{elem.lastName}</td>
+                            <td>{elem.email}</td>
+                        </tr>;
+                    })}
+                </tbody>
+            </table>
+            : <></>);
+    }, [studnts]);
+
+    return table!;
 }
 
 export default StudentCollection;
