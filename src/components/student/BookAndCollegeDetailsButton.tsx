4f0cbcec9d53096eb08166bb668c0c82104beefd
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-03-08 11:13:29 +1100

    Show full student object

--- src/components/student/BookAndCollegeDetailsButton.tsx
+++ src/components/student/BookAndCollegeDetailsButton.tsx
@@ -3,10 +3,15 @@ import { useStudentService } from "../../contexts/StudentProvider";
 const BookAndCollegeDetailsButton = () => {
     const {callBookAndCollegeDetails, firstName, id} = useStudentService();
 
+    const getBookAndCollegeDetails = (e: React.MouseEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        callBookAndCollegeDetails({studentFirstName: firstName, studentId: id});
+    };
+
     return <div>
         <input
             type="button"
-            onClick={() => callBookAndCollegeDetails({studentFirstName: firstName, studentId: id})}
+            onClick={getBookAndCollegeDetails}
             value="Submit"
         />
     </div>;
--- src/components/student/StudentCollection.tsx
+++ src/components/student/StudentCollection.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useStudentService } from "../../contexts/StudentProvider";
+import { Book } from "../../models/Book";
+import { CollegeBooks } from "../../models/CollegeBooks";
 
 const StudentCollection = () => {
     const {callStudents, studnts} = useStudentService();
@@ -9,9 +11,11 @@ const StudentCollection = () => {
         callStudents();
     }, []);
 
-    useEffect(() => {
+    const getStudentsDetails = (e: React.MouseEvent<HTMLInputElement>) => {
+        e.preventDefault();
         setTable(studnts !== undefined
             ? <table>
+                <caption>Students</caption>
                 <thead>
                     <tr>
                         <th>FirstName</th>
@@ -25,6 +29,114 @@ const StudentCollection = () => {
                             <td>{elem.firstName}</td>
                             <td>{elem.lastName}</td>
                             <td>{elem.email}</td>
+                            <td>
+                                <input
+                                type="button"
+                                onClick={(e) => getCollegeDetails(e, elem.college)}
+                                value="College"/>
+                            </td>
+                        </tr>;
+                    })}
+                </tbody>
+            </table>
+            : <></>);
+    };
+
+    const getBooksDetails = (e: React.MouseEvent<HTMLInputElement>, books: Book[]) => {
+        e.preventDefault();
+        setTable(books !== undefined
+            ? <table>
+                <caption>Books</caption>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Author</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books!.map((elem, index) => {
+                        return <tr key={elem.id}>
+                            <td>{elem.name}</td>
+                            <td>{elem.author}</td>
+                        </tr>;
+                    })}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={2}>
+                            <input
+                                type="button"
+                                onClick={(e) => getStudentsDetails(e)}
+                                value="Reset"/>
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+            : <></>);
+    };
+
+    const getCollegeDetails = (e: React.MouseEvent<HTMLInputElement>, collegeBooks: CollegeBooks) => {
+        e.preventDefault();
+        setTable(collegeBooks !== undefined
+            ? <table>
+                <caption>College</caption>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Location</th>
+                        <th>Rating</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr key={collegeBooks.id}>
+                        <td>{collegeBooks.name}</td>
+                        <td>{collegeBooks.location}</td>
+                        <td>{collegeBooks.rating}</td>
+                        <td>
+                            <input
+                            type="button"
+                            onClick={(e) => getBooksDetails(e, collegeBooks.books)}
+                            value="Books"/>
+                        </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={2}>
+                            <input
+                                type="button"
+                                onClick={(e) => getStudentsDetails(e)}
+                                value="Reset"/>
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+            : <></>);
+    };
+
+    useEffect(() => {
+        setTable(studnts !== undefined
+            ? <table>
+                <caption>Students</caption>
+                <thead>
+                    <tr>
+                        <th>FirstName</th>
+                        <th>LastName</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {studnts!.map((elem, index) => {
+                        return <tr key={elem.id}>
+                            <td>{elem.firstName}</td>
+                            <td>{elem.lastName}</td>
+                            <td>{elem.email}</td>
+                            <td>
+                                <input
+                                type="button"
+                                onClick={(e) => getCollegeDetails(e, elem.college)}
+                                value="College"/>
+                            </td>
                         </tr>;
                     })}
                 </tbody>
