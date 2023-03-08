18c332c898107b998f5454093e8817ce2ddc0433
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2023-03-08 15:49:25 +1100

    Enable toggle capability for college and books

--- src/components/student/StudentList.tsx
+++ src/components/student/StudentList.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
 import { useStudentService } from "../../contexts/StudentProvider";
-import { Book } from "../../models/Book";
-import { CollegeBooks } from "../../models/CollegeBooks";
 
 const StudentList = () => {
     const {callStudents, studnts} = useStudentService();
@@ -11,50 +9,12 @@ const StudentList = () => {
         callStudents();
     }, []);
 
-    const captionStyle = {
-        fontSize: "70px",
-        fontWeight: "600",
-        color: "#b393d3",
-        textShadow: "1px 1px 0px #957dad, 1px 2px 0px #957dad, 1px 3px 0px #957dad, 1px 4px 0px #957dad, 1px 5px 0px #957dad, 1px 6px 0px #957dad, 1px 10px 5px rgba(16, 16, 16, 0.5), 1px 15px 10px rgba(16, 16, 16, 0.4), 1px 20px 30px rgba(16, 16, 16, 0.3), 1px 25px 50px rgba(16, 16, 16, 0.2)",
-        textAlign: "center" as const,
-        textTransform: "uppercase" as const
-    };
-
-    const headerStyle = {
-        paddingTop: "12px",
-        paddingBottom: "12px",
-        backgroundColor: "#04AA6D",
-        color: "white",
-        textAlign: "left" as const
-    };
-
-    const getStudentsDetails = (e: React.MouseEvent<HTMLInputElement>) => {
-        e.preventDefault();
-        setList(studnts !== undefined
-            ? <div>get student details</div>
-            : <></>);
-    };
-
-    const getBooksDetails = (e: React.MouseEvent<HTMLInputElement>, books: Book[]) => {
-        e.preventDefault();
-        setList(books !== undefined
-            ? <div>get books details</div>
-            : <></>);
-    };
-
-    const getCollegeDetails = (e: React.MouseEvent<HTMLInputElement>, collegeBooks: CollegeBooks) => {
-        e.preventDefault();
-        setList(collegeBooks !== undefined
-            ? <div>get college details</div>
-            : <></>);
-    };
-
     const resetList = (eventTarget: HTMLInputElement) => {
         const uList = Array.from(eventTarget.parentElement?.parentElement?.children!);
         uList.forEach((elem, index) => {
             const listElement = elem as HTMLUListElement;
             const listChildren = Array.from(listElement.children);
-            const info = listChildren?.filter(s => s.classList.contains("studentInfo"));
+            const info = listChildren?.filter(s => s.classList.contains("toggle"));
             info.forEach((elem, index) => {
                 const divElement = elem as HTMLDivElement;
                 divElement.hidden = true;
@@ -64,7 +24,7 @@ const StudentList = () => {
 
     const selectListItem = (eventTarget: HTMLInputElement) => {
         const listChildren = Array.from(eventTarget.parentElement?.children!);
-        const info = listChildren?.filter(s => s.classList.contains("studentInfo"));
+        const info = listChildren?.filter(s => s.classList.contains("toggle"));
         info.forEach((elem, index) => {
             const divElement = elem as HTMLDivElement;
             divElement.hidden = !divElement.hidden;
@@ -87,24 +47,64 @@ const StudentList = () => {
                             type="button"
                             onClick={(e) => toggleInfo(e)}
                             value={"Student " + (index+1).toString()}/>
-                        <div hidden className="studentInfo">
+                        <div hidden className="toggle">
                             <span>First name: </span>
                             <span>{elem.firstName}</span>
                         </div>
-                        <div hidden className="studentInfo">
+                        <div hidden className="toggle">
                             <span>Last name: </span>
                             <span>{elem.lastName}</span>
                         </div>
-                        <div hidden className="studentInfo">
+                        <div hidden className="toggle">
                             <span>Email: </span>
                             <span>{elem.email}</span>
                         </div>
-                        <div hidden className="studentInfo">
-                            <input
-                            type="button"
-                            onClick={(e) => getCollegeDetails(e, elem.college)}
-                            value="College"/>
+                        {elem.college !== undefined
+                        ? <div className="toggle">
+                            <ul>
+                                <li key={elem.college.id}>
+                                    <input
+                                        type="button"
+                                        onClick={(e) => toggleInfo(e)}
+                                        value={"College for student " + (index+1).toString()}/>
+                                    <div hidden className="toggle">
+                                        <span>Name: </span>
+                                        <span>{elem.college.name}</span>
+                                    </div>
+                                    <div hidden className="toggle">
+                                        <span>Location: </span>
+                                        <span>{elem.college.location}</span>
+                                    </div>
+                                    <div hidden className="toggle">
+                                        <span>Rating: </span>
+                                        <span>{elem.college.rating}</span>
+                                    </div>
+                                    {elem.college.books !== undefined
+                                    ? <div className="toggle">
+                                        <ul>
+                                            {elem.college.books!.map((elem, index) => {
+                                                return <li key={elem.id}>
+                                                    <input
+                                                        type="button"
+                                                        onClick={(e) => toggleInfo(e)}
+                                                        value={"Book " + (index+1).toString()}/>
+                                                    <div hidden className="toggle">
+                                                        <span>First name: </span>
+                                                        <span>{elem.name}</span>
+                                                    </div>
+                                                    <div hidden className="toggle">
+                                                        <span>Last name: </span>
+                                                        <span>{elem.author}</span>
+                                                    </div>
+                                                 </li>
+                                            })};
+                                        </ul>
+                                    </div>
+                                    : <></>}
+                                </li>
+                            </ul>
                         </div>
+                        : <></>}
                     </li>;
                 })}
             </ul>
